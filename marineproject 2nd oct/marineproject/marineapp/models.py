2c99cf881763e358f9d024de33e7663b998c85cb
Chirag Shivakumar <86071790+chiragshiva99@users.noreply.github.com>
2021-10-08 17:04:26 +0800

    Update models.py

--- marineproject 2nd oct/marineproject/marineapp/models.py
+++ marineproject 2nd oct/marineproject/marineapp/models.py
@@ -5,28 +5,11 @@ from django.core.validators import FileExtensionValidator
 
 # Create your models here.
 
-department_choices = (
-    ('HR', 'HR'),
-    ('Trainee', 'Trainee'),
-    ('IDK', 'IDK'),
-)
-role_choices = (
-    ('role1', 'role1'),
-    ('role2', 'role2'),
-    ('role3', 'role3'),
-)
-
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField("Full Name", max_length=250)
     email = models.EmailField()
-    department = models.CharField(max_length=250,
-                                  choices=department_choices,
-                                  default='HR')
-    role = models.CharField(max_length=250,
-                            choices=role_choices,
-                            default='role1')
 
     def __str__(self):
         return self.full_name
