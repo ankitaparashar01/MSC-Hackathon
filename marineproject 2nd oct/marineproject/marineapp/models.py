f4f88a056f107585339785c681bb93efbe4d2fce
Prachi Suthar <prachi_suthar@mymail.sutd.edu.sg>
2021-10-22 13:07:06 +0800

    Update models.py

--- marineproject 2nd oct/marineproject/marineapp/models.py
+++ marineproject 2nd oct/marineproject/marineapp/models.py
@@ -7,9 +7,18 @@ from django.core.validators import FileExtensionValidator
 
 
 class Customer(models.Model):
+    DEPARTMENT_CHOICES = [
+    ('HR', 'HR'),
+    ('HOD', 'HOD'),
+    ('Trainer', 'Trainer'),
+]
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField("Full Name", max_length=250)
     email = models.EmailField()
+  #  department = models.Choices()
 
     def __str__(self):
         return self.full_name
+
+      #comments - prachi
