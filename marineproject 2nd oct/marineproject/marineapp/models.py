cf4a4098b9af79d9f811a1bcb368f685310b1158
Prachi Suthar <prachi_suthar@mymail.sutd.edu.sg>
2021-10-23 12:31:43 +0800

    Update models.py

--- marineproject 2nd oct/marineproject/marineapp/models.py
+++ marineproject 2nd oct/marineproject/marineapp/models.py
@@ -2,6 +2,8 @@ from django.core import validators
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import FileExtensionValidator
+from django.db.models.enums import Choices
+from multiselectfield import MultiSelectField
 
 # Create your models here.
 
@@ -16,13 +18,12 @@ class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField("Full Name", max_length=250)
     email = models.EmailField()
-
-    #  department = models.Choices()
+    department = MultiSelectField(choices = DEPARTMENT_CHOICES)
 
     def __str__(self):
         return self.full_name
 
 
-#comments - prachi
+#git trial - prachi
 
 #Chirag loves paneer
