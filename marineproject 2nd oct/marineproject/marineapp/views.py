665eda7a46d87150a6e77f63d55300a8e7f0b2d9
Chirag Shivakumar <86071790+chiragshiva99@users.noreply.github.com>
2021-10-24 03:12:50 +0800

    Update views.py

--- marineproject 2nd oct/marineproject/marineapp/views.py
+++ marineproject 2nd oct/marineproject/marineapp/views.py
@@ -8,6 +8,12 @@ from django.contrib.auth import authenticate, login, logout
 
 from .forms import SignUpForm, LogInForm
 
+from django.contrib.auth import get_user_model
+from verify_email.email_handler import send_verification_email  #MIGHT HAVE ERROR
+from django.views.decorators.csrf import csrf_exempt
+
+# from django_email_verification import sendConfirm
+
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -17,7 +23,7 @@ class HomeView(TemplateView):
 class SignUpView(CreateView):
     template_name = "signup.html"
     form_class = SignUpForm
-    success_url = reverse_lazy("marineapp:home")
+    success_url = reverse_lazy("marineapp:dashboard")
 
     def form_valid(self, form):
         username = form.cleaned_data.get("username")
@@ -42,6 +48,11 @@ class LogInView(FormView):
         pword = form.cleaned_data.get("password")
         usr = authenticate(username=uname, password=pword)
 
+        # if form.is_valid():
+        #     inactive_user = send_verification_email(request, form)
+        #     inactive_user.cleaned_data['email']
+        # Output : test-user123@gmail.com
+
         if usr is not None and usr.customer:
             login(self.request, usr)
         else:
@@ -62,6 +73,21 @@ class LogOutView(View):
 class DashboardView(TemplateView):
     template_name = "dashboard.html"
 
-   
 
-   
+# class VerificationView(TemplateView):
+#     #Template_name = "my_custom_email_message.html"
+#     pass
+
+
+@csrf_exempt
+def sendEmail(form, request):
+    # username = request.POST.get('username')
+    username = form.cleaned_data.get("username")
+    password = form.cleaned_data.get("password")
+    email = form.cleaned_data.get("email")
+    user = get_user_model().objects.create(username=username,
+                                           password=password,
+                                           email=email)
+    # sendConfirm(user)
+    send_verification_email(user)
+    return render(request, 'confirm_template.html')
