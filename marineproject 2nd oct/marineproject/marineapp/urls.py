86b0741feea80170c4e7eef81b4c711299f86197
Chirag Shivakumar <86071790+chiragshiva99@users.noreply.github.com>
2021-10-24 03:12:47 +0800

    Update urls.py

--- marineproject 2nd oct/marineproject/marineapp/urls.py
+++ marineproject 2nd oct/marineproject/marineapp/urls.py
@@ -1,5 +1,7 @@
-from django.urls import path
+from django.urls import path, include, re_path
 from .views import *
+# from django_email_verification import urls as mail_urls
+from verify_email import urls as mail_urls  #MIGHT HAVE ERROR
 
 app_name = "marineapp"
 urlpatterns = [
@@ -8,4 +10,7 @@ urlpatterns = [
     path("login/", LogInView.as_view(), name="login"),
     path("logout/", LogOutView.as_view(), name="logout"),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
+    # path('verification/', VerificationView.as_view(), name='verify_email'),
+    re_path('^send_email/$', sendEmail),
+    path('email/', include(mail_urls)),
 ]
