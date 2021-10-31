5663f63832c469087c53fe5cdd115e9bc8643a01
Chirag Shivakumar <86071790+chiragshiva99@users.noreply.github.com>
2021-10-31 22:50:36 +0800

    Update settings.py

--- marineproject 2nd oct/marineproject/marineproject/settings.py
+++ marineproject 2nd oct/marineproject/marineproject/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILE_DIRS = [BASE_DIR / "static"]
+# STATICFILE_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = BASE_DIR / "static_cdn"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
