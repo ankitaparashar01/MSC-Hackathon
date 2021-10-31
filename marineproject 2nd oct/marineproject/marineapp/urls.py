fc6df2882491e71cbb4d63b87e53b49d371b6e1f
Chirag Shivakumar <86071790+chiragshiva99@users.noreply.github.com>
2021-10-31 22:50:30 +0800

    Update urls.py

--- marineproject 2nd oct/marineproject/marineapp/urls.py
+++ marineproject 2nd oct/marineproject/marineapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include, re_path
 from .views import *
 from django_email_verification import urls as email_urls
-from . import views 
+from . import views
 # from verify_email import urls as mail_urls  #MIGHT HAVE ERROR
 
 app_name = "marineapp"
@@ -15,12 +15,22 @@ urlpatterns = [
     re_path('^send_email/$', sendEmail),
     path('email/', include(email_urls)),
     path("hrdashboard/", HRDashboardView.as_view(), name="hrdashboard"),
-    path("traineedashboard/", TraineeDashboardView.as_view(), name="traineedashboard"),
-    path("trainerdashboard/", TrainerDashboardView.as_view(), name="trainerdashboard"),
-    path("module1question/", Module1QuestionView.as_view(), name="module1question"),
-    path("module2question/", Module2QuestionView.as_view(), name="module2question"),
-    path("module3question/", Module3QuestionView.as_view(), name="module3question"),
-    path("delete-record/<int:id>", views.delrec)
-    
-
+    path("traineedashboard/",
+         TraineeDashboardView.as_view(),
+         name="traineedashboard"),
+    path("trainerdashboard/",
+         TrainerDashboardView.as_view(),
+         name="trainerdashboard"),
+    path("module1question/",
+         Module1QuestionView.as_view(),
+         name="module1question"),
+    path("module2question/",
+         Module2QuestionView.as_view(),
+         name="module2question"),
+    path("module3question/",
+         Module3QuestionView.as_view(),
+         name="module3question"),
+    path("delete-record/<int:id>", views.delrec),
+    path("inside_ship/", InsideShipView.as_view(), name="insideship"),
+    path("outside_ship/", OutsideShipView.as_view(), name="Outsideship")
 ]
